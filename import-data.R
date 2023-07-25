fa43c58cae3261d289ac1fe77c1e3689dee8f986
Marcos Abreu <abreums@gmail.com>
2023-07-25 12:09:18 -0300

    excluding janitor reference

--- import-data.R
+++ import-data.R
@@ -1,17 +1,10 @@
 library(tidyverse)
-library(gt)
 library(googlesheets4)
 
 gs4_deauth()
 
 survey_data <- 
-  read_sheet("https://docs.google.com/spreadsheets/d/1mjzq9i4OaTMIgd_BGRYai8aH0re7ES14wglZreOc0Po/edit?usp=sharing") |> 
-  janitor::clean_names()
+  read_sheet("https://docs.google.com/spreadsheets/d/1mjzq9i4OaTMIgd_BGRYai8aH0re7ES14wglZreOc0Po/edit?usp=sharing")
 
 survey_data |> 
   write_rds("survey-data.rds")
-
-survey_data |> 
-  gt() |> 
-  as_raw_html() |> 
-  cat(file = "file.html")
